6b4bfd400041b4b7f9020ca3bab6c3dafd4f85be
grahamlyons <graham@grahamlyons.com>
2019-08-29 16:52:59 +0100

    Add files for running all tests in Docker
    Make it simpler to run all of the tests locally, in the same was
    as on CI. Also add tests for installed shell script and Ansible
    syntax via lint tool.

---
+++ test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -euo pipefail
+
+IMAGE_ID="$(basename $(pwd))"
+
+docker image build -t "${IMAGE_ID}" .
+
+docker container run --rm --name "${IMAGE_ID}" \
+    $(tty -s && echo -t) \
+    "${IMAGE_ID}" \
+    ansible-playbook tests/test.yml -i tests/inventory --syntax-check
+
+docker container run --rm --name "${IMAGE_ID}" \
+    $(tty -s && echo -t) \
+    "${IMAGE_ID}" \
+    ansible-lint .
+
+docker container run --rm -it -v "$(pwd)":"$(pwd)" -w "$(pwd)" \
+    koalaman/shellcheck \
+    ./files/notify_updates
