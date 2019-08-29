705993feb4f841034455c75b11e2fedccaa9e830
grahamlyons <graham@grahamlyons.com>
2019-08-29 15:42:56 +0100

    Send a `stateless` tag to Datadog
    Look for an environment variable but don't fail if it's not there,
    just default to being stateless.

--- files/send_datadog_event.py
+++ files/send_datadog_event.py
@@ -12,11 +12,16 @@ def main(argv):
     updates = argv[0]
     jenkins_job_name = os.getenv('JENKINS_JOB_NAME')
 
+    stateless = bool(os.getenv('STATELESS_INSTANCE', 1))
+
     initialize(**options)
 
     title = "Software Update!"
     text = 'update-notifier (yum-cron) has flagged the following updates \n{}'.format(updates)
-    tags = ['job:{}'.format(jenkins_job_name)]
+    tags = [
+        'job:{}'.format(jenkins_job_name),
+        'stateless:{}'.format(stateless),
+    ]
 
     api.Event.create(title=title, text=text, tags=tags)
 
