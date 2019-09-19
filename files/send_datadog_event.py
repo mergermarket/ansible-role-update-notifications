d70429a370c7a75b8a8f288aa9e15a40c5efd55f
Arthur Gassner <arthur.gassner@gmail.com>
2019-09-19 17:15:08 +0100

    - Fix boolean conversion for stateless flag - Tidy up

--- files/send_datadog_event.py
+++ files/send_datadog_event.py
@@ -12,12 +12,13 @@ def main(argv):
     updates = argv[0]
     jenkins_job_name = os.getenv('JENKINS_JOB_NAME')
 
-    stateless = bool(os.getenv('STATELESS_INSTANCE', 1))
+    stateless = bool(int(os.getenv('STATELESS_INSTANCE', 1)))
 
     initialize(**options)
 
     title = "Software Update!"
-    text = 'update-notifier (yum-cron) has flagged the following updates \n{}'.format(updates)
+    text = 'update-notifier (yum-cron) has flagged ' \
+           'the following updates \n{}'.format(updates)
     tags = [
         'job:{}'.format(jenkins_job_name),
         'stateless:{}'.format(stateless),
